1b4a1afa5d0f317893c0986335f73c743bfece29
David Rodriguez <dsebastianrodriguez@ucundinamarca.edu.co>
2021-09-20 18:31:05 -0500

    Documento base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { DepartamentosComponent } from './pages/departamentos/departamentos.component';
+import { EditarComponent } from './pages/editar/editar.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'buscar', component: BuscarComponent},
+  {path: 'editar', component: EditarComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'departamentos', component: DepartamentosComponent}
+  //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule} from './material/material.module';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { EditarComponent } from './pages/editar/editar.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { DepartamentosComponent } from './pages/departamentos/departamentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    BuscarComponent,
+    EditarComponent,
+    DepartamentosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
